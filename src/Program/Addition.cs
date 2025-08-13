7c9c692d188668e688563f719d91ba88b7df10c1
Rogueair <144634618+Rogueair@users.noreply.github.com>
2025-08-13 11:57:06 -0300

    Last Commit

--- src/Program/Addition.cs
+++ src/Program/Addition.cs
@@ -1,9 +1,13 @@
+using System.Runtime.InteropServices;
+
 namespace Ucu.Poo.GitChallenge;
 
 public class Addition
 {
     public static int Add(int a, int b)
     {
+        Console.WriteLine("Hola");
         return a + b;
+        
     }
 }
